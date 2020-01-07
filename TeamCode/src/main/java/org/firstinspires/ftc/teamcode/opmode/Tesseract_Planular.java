a6ae9f3f50e5e19870eb0aecfa626bc54b70d139
NitrousoxideN2O <26368519+NitrousoxideN2O@users.noreply.github.com>
2020-01-07 01:17:21 -0500

    Partially completed OP Mode (check desc)
    It's almost complete but there's an error I just cant seem to fix. (WIP)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Tesseract_Planular.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Tesseract_Planular.java
@@ -9,22 +9,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.RobotCore;
+import org.firstinspires.ftc.teamcode.translator.Planular;
 
 @TeleOp(name="Tesseract_Planular")
 public class Tesseract_Planular extends OpMode {
     private RobotCore robot = new RobotCore();
 
-    private final double DEADZONERADIUS = 0.08;
-    private final double CONTROLEXPONENT = 2;
+    // TO CHANGE THESE GO TO THE CORRESPONDING TRANSLATOR.
+    private final double DEADZONERADIUS = 0.08; // DO NOT CHANGE
+    private final double CONTROLEXPONENT = 2; // DO NOT CHANGE
 
     @Override
     public void init() {
         robot.init(hardwareMap);
+        robot.setTranslator(Planular.class);
     }
 
     @Override
     public void loop() {
-        
+        robot.gamepadDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.right_stick_y);
 
         // Visualize scale: https://www.desmos.com/calculator/0xtjtrmqfk
         telemetry.addData("Left stick: ", "X[%d], Y[%d]", gamepad1.left_stick_x, gamepad1.left_stick_y);
